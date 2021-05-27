f93009892d1c29bfff6795ebee662b1942d59916
Iniya Kalaiselvan <48144802+Iniya1209@users.noreply.github.com>
2021-05-27 14:58:42 +0530

    Add files via upload

---
+++ RangeOfElements.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class RangeOfElements {
+    static int rangeFinder(int[] arr,int noOfElements){
+        Arrays.sort(arr);
+        int range=arr[noOfElements-1]-arr[0];
+        return range;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int noOfElements=sc.nextInt();
+        int[] arr=new int[noOfElements];
+        for(int i=0;i<noOfElements;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("The range of the array is "+rangeFinder(arr, noOfElements));
+        sc.close();
+    }
+}
