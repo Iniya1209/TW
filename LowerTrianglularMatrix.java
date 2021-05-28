07f28bb6e742ef30c8dbf33aea193f801f033f73
Iniya Kalaiselvan <48144802+Iniya1209@users.noreply.github.com>
2021-05-28 18:40:32 +0530

    Add files via upload

---
+++ LowerTrianglularMatrix.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class LowerTriangle {
+    static boolean isLowerTriangle(int square[][],int rowSize){
+        for(int row=0;row<rowSize-1;row++){
+            for(int col=row+1;col<rowSize;col++){
+                if(square[row][col]!=0)
+                    return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int rowSize=sc.nextInt();
+        int colSize=sc.nextInt();
+        if(rowSize!=colSize || rowSize<=0){
+            System.out.println("Invalid Input");
+        }
+        else{
+            int square[][]=new int[rowSize][colSize];
+            for(int row=0;row<rowSize;row++){
+                for(int col=0;col<colSize;col++){
+                    square[row][col]=sc.nextInt();
+                }
+            }
+            sc.close();
+            if(isLowerTriangle(square, rowSize))
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+        }
+    }
+}
